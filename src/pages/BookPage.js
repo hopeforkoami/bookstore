1084c29b3fa3845c36efe85bdcb3ee00c1e01da4
hopeforkoami <hopeforkoami@gmail.com>
2022-12-15 17:01:05 +0000

    Display books from Redux store

--- src/pages/BookPage.js
+++ src/pages/BookPage.js
@@ -1,52 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AddBook from '../Components/AddBook';
 import BookList from '../Components/BookList';
 import Header from '../Components/Header';
 
-const bookData = [
-  {
-    id: 1,
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    nbreChp: 25,
-    crtChp: 10,
-    category: 'Action',
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    author: 'Frank Herbert',
-    nbreChp: 40,
-    crtChp: 8,
-    category: 'Science Fiction',
-  },
-  {
-    id: 3,
-    title: 'Test 3 Book',
-    author: 'test3 Author',
-    nbreChp: 15,
-    crtChp: 5,
-    category: 'test Category',
-  },
-  {
-    id: 4,
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    nbreChp: 60,
-    crtChp: 0,
-    category: 'Economy',
-  },
-];
-const BookPage = () => (
-  <div className="container">
-    <Header />
-    <section className="section">
-      <BookList books={bookData} />
-      <div className="grand_divider" />
-      <AddBook />
-    </section>
+const BookPage = () => {
+  const bookData = useSelector((state) => state.books);
+  return (
+    <div className="container">
+      <Header />
+      <section className="section">
+        <BookList books={bookData} />
+        <div className="grand_divider" />
+        <AddBook />
+      </section>
 
-  </div>
-);
+    </div>
+  );
+};
 
 export default BookPage;
--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -2,7 +2,40 @@
 export const addBookAction = (bookObject) => ({ type: 'BOOK_ADDED', payload: bookObject });
 export const removeBookAction = (bookID) => ({ type: 'BOOK_REMOVED', payload: bookID });
 const initStateAction = { type: 'STATE_INITIALIZED' };
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    nbreChp: 25,
+    crtChp: 10,
+    category: 'Action',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+    nbreChp: 40,
+    crtChp: 8,
+    category: 'Science Fiction',
+  },
+  {
+    id: 3,
+    title: 'Test 3 Book',
+    author: 'test3 Author',
+    nbreChp: 15,
+    crtChp: 5,
+    category: 'test Category',
+  },
+  {
+    id: 4,
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    nbreChp: 60,
+    crtChp: 0,
+    category: 'Economy',
+  },
+];
 // reducer
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
